a9e62896fe1569f8b94be17078c3053e13940a23
Caique Alves <calves@mundolivre.digital>
2021-11-17 21:04:49 -0300

    adicionando tratamento de erro

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const chalk = require('chalk')
 const fs =require('fs') 
-const caminho = './arquivo/text1.md'
+const caminho = './arquivo/'
+
+function TratarErro(erro){
+    if(erro.code==='EISDIR'){
+
+        throw new Error(chalk.red('não há arquivos no caminho escrito'))
+    }else{
+        throw new Error(chalk.red('ocorreu um erro na leitura, o caminho do arquivo pode estar errado'))
+
+    }
+}
 
 function PegaArquivo(caminho){
     let encode = 'utf-8'
-    fs.readFile(caminho,encode,(_,Dados)=>{
+    fs.readFile(caminho,encode,(erro,Dados)=>{
+        if(erro){
+            TratarErro(erro)
+        }
         console.log(chalk.green(Dados))
     })
  
